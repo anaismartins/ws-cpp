4a496981e41593921ea0412d885e20601a431e68
ana <anaisabelmartins@tecnico.ulisboa.pt>
2022-03-22 11:30:10 +0000

    first program

---
+++ code/hello_world_v1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+
+    printf("hello world");
+
+    return 0;
+}
---
+++ code/hello_world_v2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "hello world";
+
+    return 0;
+}
